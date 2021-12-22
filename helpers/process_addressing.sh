7963b279c642dee476d55851faef03a39cfbd030
PLAS Glenn <gplas@cibg.brussels>
2021-12-22 10:55:54 +0100

    fix home

--- helpers/process_addressing.sh
+++ helpers/process_addressing.sh
@@ -12,7 +12,7 @@ echo ""
 echo "${GREEN}Addressing${RESET}"
 echo "======"
 
-cd ~
+cd ~${DEPLOY_USER}
 
 # address directly in the database using DBF database file, the tool will take care of all anomalities encountered (knw/Gbg)
 php grb2osm/grb2osm.php -f /usr/local/src/grb/GRBgis_20001/Shapefile/TblGbgAdr20001B500.dbf,/usr/local/src/grb/GRBgis_10000/Shapefile/TblGbgAdr10000B500.dbf,/usr/local/src/grb/GRBgis_30000/Shapefile/TblGbgAdr30000B500.dbf,/usr/local/src/grb/GRBgis_40000/Shapefile/TblGbgAdr40000B500.dbf,/usr/local/src/grb/GRBgis_70000/Shapefile/TblGbgAdr70000B500.dbf,/usr/local/src/grb/GRBgis_30000/Shapefile/TblKnwAdr30000B500.dbf,/usr/local/src/grb/GRBgis_70000/Shapefile/TblKnwAdr70000B500.dbf,/usr/local/src/grb/GRBgis_20001/Shapefile/TblKnwAdr20001B500.dbf,/usr/local/src/grb/GRBgis_40000/Shapefile/TblKnwAdr40000B500.dbf
