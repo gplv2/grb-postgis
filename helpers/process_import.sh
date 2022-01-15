64a70758c66ebc1f094018efa27ff0b591ab3474
PLAS Glenn <gplas@cibg.brussels>
2022-01-15 22:23:24 +0100

    fix about 15051 streetnames on buildings with wrong spelling

--- helpers/process_import.sh
+++ helpers/process_import.sh
@@ -140,6 +140,7 @@ UPDATE planet_osm_polygon SET building='yes' WHERE building='Category 2';
 UPDATE planet_osm_polygon SET "source:geometry:date" = concat_ws('-',substring("source:geometry:date",1,4), substring("source:geometry:date",5,2), substring("source:geometry:date",7,2)) WHERE "source:geometry:entity"='Picc';
 UPDATE planet_osm_polygon SET way=ST_centroid(way) WHERE man_made='mast';
 UPDATE planet_osm_polygon SET osm_id=abs(osm_id) WHERE osm_id<0;
+UPDATE planet_osm_polygon SET "addr:street" = upper(left("addr:street", 1)) || right("addr:street", -1) WHERE "addr:street" is not null AND "addr:street" <> upper(left("addr:street", 1)) || right("addr:street", -1) AND "addr:street" NOT IN ('von Asten-Straße','von-Dhaem-Strasse','von-Montigny-Straße','von-Orley-Straße','t Zand','be-MINE') AND position('''' IN "addr:street") <> 2;
 EOF
 
 # These are primarily if you hook up a bbox client script to it, not really interesting when all you want to do is export the built database to a file
