628cf141ae3e625ef3baefeda886a24e0c1d7551
Glenn Plas <glenn@bitless.be>
2022-01-25 20:28:38 +0100

    update fix list

--- helpers/fix_streetnames.sql
+++ helpers/fix_streetnames.sql
@@ -1,1 +1,5 @@
 update planet_osm_polygon set "addr:street"='Rue de l''Équerre - Winkelhaakstraat' WHERE "addr:street" = 'Rue de l''Equerre - Winkelhaakstraat';
+update planet_osm_polygon set "addr:street:fr"='Rue de l''Équerre' WHERE "addr:street:fr" = 'Rue de l''Equerre';
+update planet_osm_polygon set "addr:street"='Rue Docteur Élie Lambotte - Dokter Élie Lambottestraat' WHERE "addr:street" = 'Rue Docteur Elie Lambotte - Dokter Elie Lambottestraat';
+update planet_osm_polygon set "addr:street:fr"='Rue Docteur Élie Lambotte' WHERE "addr:street:fr" = 'Rue Docteur Elie Lambotte';
+update planet_osm_polygon set "addr:street"='Rue Ducale - Hertogstraat' , "addr:street:nl"='Hertogstraat' WHERE "addr:street" = 'Rue Ducale - Hertogsstraat';
