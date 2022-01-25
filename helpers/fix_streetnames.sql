d725fe0ce2cc81e3263b7eb930c6c8dafe152b27
Glenn Plas <glenn@bitless.be>
2022-01-25 20:35:33 +0100

    update fix list

--- helpers/fix_streetnames.sql
+++ helpers/fix_streetnames.sql
@@ -3,3 +3,5 @@ update planet_osm_polygon set "addr:street:fr"='Rue de l''Équerre' WHERE "addr:
 update planet_osm_polygon set "addr:street"='Rue Docteur Élie Lambotte - Dokter Élie Lambottestraat' WHERE "addr:street" = 'Rue Docteur Elie Lambotte - Dokter Elie Lambottestraat' AND "source:geometry:entity"='Urbis';
 update planet_osm_polygon set "addr:street:fr"='Rue Docteur Élie Lambotte' WHERE "addr:street:fr" = 'Rue Docteur Elie Lambotte' AND "source:geometry:entity"='Urbis';
 update planet_osm_polygon set "addr:street"='Rue Ducale - Hertogstraat' , "addr:street:nl"='Hertogstraat' WHERE "addr:street" = 'Rue Ducale - Hertogsstraat' AND "source:geometry:entity"='Urbis';
+update planet_osm_polygon set "addr:street"='Rue de la Cigogne - Ooievaarstraat' , "addr:street:nl"='Ooievaarstraat' WHERE "addr:street" = 'Rue de la Cigogne - Ooievaarsstraat' AND "source:geometry:entity"='Urbis';
+update planet_osm_polygon set "addr:street"='Rue des Œillets - Anjelierenstraat' , "addr:street:fr"='Rue des Œillets' WHERE "addr:street" = 'Rue des Oeillets - Anjelierenstraat' AND "source:geometry:entity"='Urbis';
