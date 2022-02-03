77d82f2ebf848acb0cb00910cf87a40357dd2dd3
Glenn Plas <glenn@bitless.be>
2022-02-03 17:09:56 +0100

    fix nl part

--- helpers/fix_streetnames.sql
+++ helpers/fix_streetnames.sql
@@ -9,5 +9,6 @@ UPDATE planet_osm_polygon SET "addr:street"='Rue de la Cigogne - Ooievaarstraat'
 UPDATE planet_osm_polygon SET "addr:street"='Rue de l''Équerre - Winkelhaakstraat' WHERE "addr:street" = 'Rue de l''Equerre - Winkelhaakstraat' AND "source:geometry:entity"='Urbis';
 UPDATE planet_osm_polygon SET "addr:street"='Rue des Œillets - Anjelierenstraat' , "addr:street:fr"='Rue des Œillets' WHERE "addr:street" = 'Rue des Oeillets - Anjelierenstraat' AND "source:geometry:entity"='Urbis';
 UPDATE planet_osm_polygon SET "addr:street"='Rue Docteur Élie Lambotte - Dokter Élie Lambottestraat' WHERE "addr:street" = 'Rue Docteur Elie Lambotte - Dokter Elie Lambottestraat' AND "source:geometry:entity"='Urbis';
+UPDATE planet_osm_polygon SET "addr:street:nl"='Dokter Élie Lambottestraat' WHERE "addr:street" = 'Rue Docteur Élie Lambotte - Dokter Élie Lambottestraat' AND "source:geometry:entity"='Urbis';
 UPDATE planet_osm_polygon SET "addr:street"='Rue Ducale - Hertogstraat' , "addr:street:nl"='Hertogstraat' WHERE "addr:street" = 'Rue Ducale - Hertogsstraat' AND "source:geometry:entity"='Urbis';
 UPDATE planet_osm_polygon SET "addr:street"='Val des Épinettes - Doorndaal' , "addr:street:fr"='Val des Épinettes' WHERE "addr:street" = 'Val des Epinettes - Doorndaal' AND "source:geometry:entity"='Urbis';
